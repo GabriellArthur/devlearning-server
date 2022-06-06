667d77089c36462198e14619371c68d574643044
Gabriel Fiusa <gabriel.arthur2@gmail.com>
2022-06-06 20:36:48 -0300

    Crud Complete User

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,13 +1,15 @@
 import express from 'express';
 const router = express.Router();
 
-import { signin, signup, googleSignIn } from '../controllers/user.js'
+import { signin, signup, googleSignIn, getUser, updateUser, deleteUser, getUsers } from '../controllers/user.js'
+
+router.get("/", getUser);
+router.get("/all", getUsers);
+router.patch("/", updateUser);
+router.delete("/", deleteUser);
 
 router.post("/signup", signup);
 router.post("/signin", signin);
 router.post("/googleSignIn", googleSignIn);
-router.get("/", (req, res) => {
-    res.send("Helcome to Courses API - ");
-})
 
 export default router;
