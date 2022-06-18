60829f38387d1da36e5e9db1bb0e5d54810b4ff3
Gabriel Fiusa <gabriel.arthur2@gmail.com>
2022-06-18 13:56:24 -0300

    Add Dados no Cookies

--- src/models/user.js
+++ src/models/user.js
@@ -1,29 +1,32 @@
 import mongoose from "mongoose";
 
 const userSchema = mongoose.Schema({
-   id: {
-      type: String,
-   },
-   name: {
-      type: String,
-      require: true,
-   },
-   email: {
-      type: String,
-      require: true,
-   },
-   password: {
-      type: String,
-      require: false,
-   },
-   phonenumber: {
-      type: String,
-      require: false,
-   },
-   googleId: {
-      type: String,
-      require: false,
-   },
+  id: {
+    type: String,
+  },
+  name: {
+    type: String,
+    require: true,
+  },
+  email: {
+    type: String,
+    require: true,
+  },
+  password: {
+    type: String,
+    require: false,
+  },
+  phonenumber: {
+    type: String,
+    require: false,
+  },
+  googleId: {
+    type: String,
+    require: false,
+  },
+  token: {
+    type: String,
+  }
 })
 
 export default mongoose.model("User", userSchema);
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,8 +1,9 @@
 import express from 'express';
 const router = express.Router();
 
-import { signin, signup, googleSignIn } from '../controllers/user.js'
+import { getUser, googleSignIn, signin, signup } from '../controllers/user.js';
 
+router.get("/", getUser);
 router.post("/signup", signup);
 router.post("/signin", signin);
 router.post("/googleSignIn", googleSignIn);
